07779c5ceb3277988cc6424b743f47d0fe796de4
mjfrancki <mikejfrancki@gmail.com>
2019-11-06 11:42:56 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-y1e7wz-ezqwra
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-y1e7wz-ezqwra)
---
+++ src/app/cdk-drag-drop-overview-example.ts
@@ -0,0 +1,16 @@
+import {Component} from '@angular/core';
+
+/**
+ * @title Basic Drag&Drop
+ */
+@Component({
+  selector: 'cdk-drag-drop-overview-example',
+  templateUrl: 'cdk-drag-drop-overview-example.html',
+  styleUrls: ['cdk-drag-drop-overview-example.css'],
+})
+export class CdkDragDropOverviewExample {}
+
+
+/**  Copyright 2019 Google LLC. All Rights Reserved.
+    Use of this source code is governed by an MIT-style license that
+    can be found in the LICENSE file at http://angular.io/license */
